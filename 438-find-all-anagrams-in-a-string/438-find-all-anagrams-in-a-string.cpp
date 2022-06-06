a7580fb21e8278fe2f13240ded7596a6bd62bd14
n0bey0ndxd <68237298+n0bey0ndxd@users.noreply.github.com>
2022-06-06 16:35:30 +0500

    Time: 19 ms (61.10%), Space: 8.7 MB (72.59%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -4,43 +4,33 @@ public:
         if (s.size() < p.size()) {
             return {};
         }
-        std::unordered_map<char, int> m_p;
-        std::unordered_map<char, int> m_s;
+
+        std::vector<int> m_p(26, 0);
+        std::vector<int> m_s(26, 0);
         vector<int> res;
 
         for (char c : p) {
-            m_p[c]++;
+            m_p[c - 'a']++;
         }
 
         int k = p.size() - 1;
 
-        for (int i = 0; i < s.size() - k; i++) {
-            if (m_s.empty()) {
-                int j = i;
-                for (; j <= i + k; j++) {
-                    m_s[s[j]]++;
-                }
-                //continue;
-            }
-            else {
-                m_s[s[i + k]]++;
-            }
+        for (int j = 0; j < k; j++) {
+            m_s[s[j] - 'a']++;
+        }
 
-            if (m_p.find(s[i + k]) == m_p.end()) {
-                m_s.clear();
-                i += k;
-                continue;
-            }
+        for (int i = 0; i < s.size() - k; i++) {
+            m_s[s[i + k] -'a']++;
 
             bool flag = true;
-            for (const auto& item : m_p) {
-                if (item.second != m_s[item.first]) {
+            for (int i = 0; i < m_p.size(); i++) {
+                if (m_p[i] != m_s[i]) {
                     flag = false;
                     break;
                 }
             }
 
-            m_s[s[i]]--;
+            m_s[s[i] - 'a']--;
 
             if (flag) {
                 res.push_back(i);
