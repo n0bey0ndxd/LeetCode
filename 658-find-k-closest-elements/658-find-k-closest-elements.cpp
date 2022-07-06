740d6412479008cd43ff8f08b602230296369aec
n0bey0ndxd <68237298+n0bey0ndxd@users.noreply.github.com>
2022-07-06 13:15:34 +0500

    Time: 47 ms (90.20%), Space: 31.1 MB (79.36%) - LeetHub

---
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    std::vector<int> findClosestElements(std::vector<int>& arr, int k, int x) {
+        int left = 0;
+        int right = k;
+        int end = arr.size();
+
+        while (right < end) {
+            int mid = (right + end) / 2;
+            int n = mid - right;
+
+            if (abs(arr[left + n] - x) > abs(arr[mid] - x)
+                || arr[mid] < x) {
+                left += n + 1;
+                right = mid + 1;
+            }
+            else {
+                end = mid;
+            }
+        }
+
+        return std::vector<int>(arr.begin() + left, arr.begin() + right);
+    }
+};
